fb2867aad218b13c228f01b999d2c233275e560e
Forillusion <78995328+Forillusion@users.noreply.github.com>
2021-07-03 16:33:51 +0800

    1.0.2

--- live2d/js/message.js
+++ live2d/js/message.js
@@ -128,10 +128,18 @@ function initLive2d (){
     $('.hide-button').fadeOut(0).on('click', () => {
         $('#landlord').css('display', 'none')
     })
+	$('.switch-button').fadeOut(0).on('click', () => {
+        loadlive2d("live2d", "https://cdn.jsdelivr.net/gh/Forillusion/Forillusion@v1.0.1/live2d/model/pio/model.json");
+    })
     $('#landlord').hover(() => {
         $('.hide-button').fadeIn(600)
     }, () => {
         $('.hide-button').fadeOut(600)
+    })
+	$('#landlord').hover(() => {
+        $('.switch-button').fadeIn(600)
+    }, () => {
+        $('.switch-button').fadeOut(600)
     })
 }
 initLive2d ();
